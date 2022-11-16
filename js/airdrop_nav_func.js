ede341b91876da7aa2d5f4f9f29d1ad8c122ecce
Philip Billson <philip_billson@hotmail.co.uk>
2022-11-16 17:33:34 +0000

    Active/Inactive Airdrop Tab Nav CSS/JS Complete

--- js/airdrop_nav_func.js
+++ js/airdrop_nav_func.js
@@ -1,30 +1,37 @@
-function airdrop_Tab(button_Name, event) 
+function airdrop_Tab(button_Name) 
 {
     console.log("Testing 123!")
-   console.log(event, '<<<<- EVENT!')
+   
     const airdrop = document.getElementsByClassName("airdrop_sections");
         for (let i = 0; i < airdrop.length; i++) {
       airdrop[i].style.display = "none";
     }
     document.getElementById(button_Name).style.display = "block";
 
-    let prevButton = null;
-    const element = document.getElementById("airdrop-section_btns");
-    element.addEventListener('click',(e) =>{
-        const isButton = e.target.nodeName === 'BUTTON';
-        if (!isButton){
-            return;
-        }
-        e.target.classList.add('air_drop_tab_btn_active');
-        if(prevButton !== null){
-            prevButton.classList.remove('air_drop_tab_btn_active')
-        }
-        prevButton = e.target;
-    })
+    console.log(button_Name)
 
-      document.getElementById(button_Name).style.display = "block";
 
-    console.log(element)
+// Get the container element
+var btnContainer = document.getElementById("airdropBTNS");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("air_drop_tab_btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}  
+
     console.log("Finished Testing 123!")
 
 
